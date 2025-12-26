4d1970a0790371af7f4a115a06c26450a7c96b5b
Eric Li <lieric446@gmail.com>
2025-12-25 21:33:54 -0500

    misspelling

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
     return (
         <div className="custom-container">
             <div className="flex justify-between h-24">
-                <h1>Projects too be updated as I work on stuff...</h1>
+                <h1>Projects to be updated as I work on stuff...</h1>
             </div>
         </div>
     );
