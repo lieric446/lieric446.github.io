76a178e45245449bed409a2ad2febae0efb3e007
Eric Li <lieric446@gmail.com>
2025-12-25 21:30:55 -0500

    base website template

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,7 +2,7 @@ export default function Notes() {
     return (
         <div className="custom-container">
             <div className="flex justify-between h-24">
-                <h1>Notes Page</h1>
+                <h1>Notes to be updated as my thoughts occur...</h1>
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,24 @@ export default function Home() {
         <br/>
         Wanted to make a page as a record for stuff as I tinker around with things. Feel free to reach out!
       </div>
+
+      <br/><br/><br/>
+
+      <h2 className="text-3xl font-semibold">Current Interests:</h2>
+      <ul className="list-disc list-inside mt-2 ml-8 text-lg">
+        <li>Computer Vision</li>
+        <li>Linguistics</li>
+        <li>Tender Is the Night by F. Scott Fitzgerald</li>
+      </ul>
+
+      <br/><br/>
+
+      <h2 className="text-3xl font-semibold">Experience</h2>
+      <ul className="list-disc list-inside mt-2 ml-8 text-lg">
+        <li>Software Engineer at Dell Technologies (Since July 2023)</li>
+        <li>Teaching Assistant at NCSU</li>
+        <li>Undergraduate Research Assistant at NCSU</li>
+      </ul>
     </div>
   );
 }
